2f796de85bb37138209f3bb935adcb8a01346273
iwatsuru <sao3104s@gmail.com>
2019-09-12 13:51:46 +0900

    標準表示されている名前を押すと、ログアウトするがログアウト後にブラウザバック不可能に

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,7 +12,7 @@
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items>
-        <v-btn class="title" nuxt to="login" text>{{ account | name }}様</v-btn>
+        <v-btn class="title" @click="login" text>{{ account | name }}様</v-btn>        
       </v-toolbar-items>
     </v-app-bar>
     <v-content>
@@ -38,5 +38,11 @@ export default {
   computed: {
     ...mapGetters("login", ["account", "branch", "bank"]),
   },
+   methods:{
+    login(){
+      this.$router.push("login");
+      location.replace("login");
+    }
+  }
 };
 </script>
